baaf26e0f23af9a8272f30850181954e9db90598
Vera Pavlyuk <vera.pavlyuk@onereach.ai>
2025-10-04 20:28:56 +0300

    chore: fixes

--- Task1/src/index.ts
+++ Task1/src/index.ts
@@ -1,10 +1,13 @@
 const name: string = "John";
 const dateBirth: string| Date = new Date("1990-01-01");
 const phoneNumber: string|number = "+380991234567";
-const children: null = null;
-const zip: undefined = undefined;
+const children: null | number = null;
+const zip: undefined | number = undefined;
 
-const printUserInfo = (...args: [unknown?, unknown?, unknown?, unknown?]): void => {
+type funcArgument = string | Date | number | null | undefined;
+type funcArguments = [funcArgument?, funcArgument?, funcArgument?, funcArgument?];
+
+const printUserInfo = (...args: funcArguments): void => {
     console.log('User info:', args.join(', '));
 }
 
